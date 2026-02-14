d76827009d93647c21cfe91f3d19f81ad295226d
Jozsef Acosta R <jozsefacosta@gmail.com>
2026-02-14 14:20:16 -0500

    change name var

--- gen-code-api/server.js
+++ gen-code-api/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.GITHUB_TOKEN
 });
 
 app.post("/generate-constructor", async (req, res) => {
